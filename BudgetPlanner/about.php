25ead9fb15617ae4f3bc6b0e3ca4d1889962d075
Gwynette2004 <157551832+Gwynette2004@users.noreply.github.com>
2024-05-19 23:47:16 +0800

    edited about page

--- BudgetPlanner/about.php
+++ BudgetPlanner/about.php
@@ -32,6 +32,7 @@ if (mysqli_num_rows($result) > 0) {
     <title>CenTipid About</title>
     <link rel="icon" type="image/x-icon" href="image/Cent-Tipid.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     
 </head>
 <body>
@@ -55,7 +56,83 @@ if (mysqli_num_rows($result) > 0) {
 
         <div class="img_logo">
             <img src="image/Cent-Tipid(nobg).png" alt="CenTipid Logo">
+            <h1>CenTipid</h1>
         </div>
+
+        <div class="about">
+            <h1>About Us</h1>
+            <p>
+            This project aims to help people, especially those with low incomes,
+            overcome common budgeting challenges. Many struggle to organize, track, 
+            and manage their money, leading to financial instability and stress.
+            Without a solid budgeting plan, they risk falling into debt and
+            failing to save for important goals like retirement or education.
+            By addressing these issues, the project empowers individuals
+            to take control of their finances and build a more secure future.
+            </p>
+        </div>
+
+        <div class="card_container">
+            <h1>Members</h1>
+            <div class="boni">
+                <img class="pfp" src="/image/Boni.png">
+                <h2>Angelo Syrean B. Bonifacio</h2>
+                <p>
+                My name is Angelo Syrean Bonifacio, I'm 20 years old, studying Information Technology
+                at Gordon College, and I always prioritize my tasks and academic requirements
+                 despite having various hobbies.
+                </p>
+            </div>
+
+            <div class="gwen">
+                <img class="pfp" src="/image/Gwen.jpg">
+                <h2>Gwynette Princess E. Rivera</h2>
+                <p>
+                I am a creative individual confident in various fields, including UI/UX design,
+                Microsoft Office, and public speaking. My diverse skill set allows me to create
+                intuitive user interfaces, manage information efficiently, and deliver engaging
+                presentations. 
+                </p>
+            </div>
+
+            <div class="jape">
+                <img class="pfp" src="/image/Jape.png">
+                <h2>Jhon Patrick G. Dela Cruz</h2>
+                <p>
+                I approach each project with passion and dedication, 
+                ensuring meticulous attention to detail and efficient completion.
+                My commitment to excellence drives me to consistently deliver
+                high-quality results in every endeavor.
+                </p>
+            </div>
+
+            <div class="andrea">
+                <img class="pfp" src="/image/Andrea.jpg">
+                <h2>Jasmin Andrea A. Turalba</h2>
+                <p>
+                A determined full-stack developer dedicated to achieving goals
+                both frontend and backend development. With my skill set and
+                unwavering commitment, I tackle challenges head-on to deliver innovative 
+                solutions.
+                </p>
+            </div>
+            
+            <div class="edmar">
+                <img class="pfp" src="/image/Edmar.png">
+                <h2>Edmar M.<br>Pantoja</h2>
+                <p>
+                I effortlessly blend social interactions with task completion. 
+                Known for my adaptable nature, I reliably meet deadlines and
+                achieve objectives with ease and efficiency, fostering a 
+                harmonious balance between work and social life.
+                </p>
+            </div>
+         </div>
+
+            <div class="contacts">
+                <h3>Contact Us</h3>
+                <p>099x-xxxx-xxx ● CenTipid@gmail.com ● <br> 321 xxxx, Olongapo City</p>
+            </div>
 
 </header>
 </body>
